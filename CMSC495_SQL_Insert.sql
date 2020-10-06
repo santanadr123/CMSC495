003a8ab27056f52c2f09bc40dfc9b0227d80c807
Gabrielle Jeuck <g.winsor629@gmail.com>
2020-10-06 10:53:51 -0700

    Add files via upload
    Final SQL Scripts for DB

---
+++ CMSC495_SQL_Insert.sql
@@ -0,0 +1,65 @@
+--Insert into Cars (10):
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (2, 'Convertible', 'Mazda', 'Miata', 2020, 32);
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (4, 'Convertible', 'Ford', 'Mustang', 2020, 38);
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (4, 'Convertible', 'Chevy', 'Camaro', 2020, 36);
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (8, 'SUV', 'Honda', 'Pilot', 2020, 30);
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (7, 'SUV', 'Mercedes-Benz', 'GLS', 2020, 42);
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (7, 'SUV', 'Nissan', 'Rogue', 2018, 32);
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (5, 'Sedan', 'Mazda', '3', 2020, 30);
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (5, 'Sedan', 'Honda', 'Civic', 2020, 31);
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (5, 'Sedan', 'Toyota', 'Camry', 2018, 29);
+INSERT INTO Cars (PassengerCapacity, CarType, Make, Model, CarYear, Price)
+VALUES (4, 'Truck', 'Toyota', 'Tundra', 2020, 48);
+
+--Insert into Hotels (10):
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (2, 1, 'King', 'TV, Private Bathroom, Safe', 142);
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (2, 1, 'Queen', 'TV, Private Bathroom, Safe', 121);
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (2, 1, 'Full', 'TV, Private Bathroom, Safe', 102);
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (4, 1, 'King Suite', 'TV, Private Bathroom, Safe, Private Hot Tub', 202);
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (4, 2, 'Double King', 'TV, Private Bathroom, Safe', 162);
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (3, 2, 'Double Queen', 'TV, Private Bathroom, Safe, Pull-out Couch', 132);
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (2, 2, 'Double Full', 'TV, Private Bathroom, Safe', 112);
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (3, 1, 'Queen Suite', 'TV, Private Bathroom, Safe, Large Seperate Shower', 102);
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (2, 1, 'Queen', 'TV, Private Bathroom, Safe', 121);
+INSERT INTO Hotels (RoomCapacity, BedNumber, BedType, Features, Price)
+VALUES (2, 1, 'Full', 'TV, Private Bathroom, Safe', 102);
+
+--Insert into Airlines (10):
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Oscar Airlines', '1st', '2020-10-18', '2020-10-18', 1000, 150, 0);
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Oscar Airlines', 'Premium', '2020-10-18', '2020-10-18', 600, 250, 0);
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Oscar Airlines', 'Economic', '2020-10-18', '2020-10-18', 342, 400, 0);
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Northwest Airlines', '1st', '2020-10-18', '2020-10-18', 1000, 150, 0);
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Northwest Airlines', 'Premium', '2020-10-18', '2020-10-18', 600, 250, 0);
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Northwest Airlines', 'Economic', '2020-10-18', '2020-10-18', 342, 400, 0);
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Allied Airlines', '1st', '2020-10-18', '2020-10-18', 1000, 150, 0);
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Allied Airlines', 'Premium', '2020-10-18', '2020-10-18', 600, 250, 0);
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Allied Airlines', 'Economic', '2020-10-18', '2020-10-18', 342, 400, 0);
+INSERT INTO Airlines (Airline, FlightClass, DepartureDate, ArrivalDate, Price, QuantityAvailable, QuantityReserved)
+VALUES ('Private Charter', '1st', '2020-10-18', '2020-10-18', 2000, 150, 0);
